f756cd109eeaf2ad28535d85211a48a287c7c794
wanglisen1 <1347915884@qq.com>
2020-04-02 05:34:09 +0000

    421334

--- public/flogin.php
+++ public/flogin.php
@@ -1,7 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+<title>请先登陆</title>
+</head>
+<body style="text-align:center;margin-top:200px;">
+	<img src="/logints.png">
+</body>
+</html>
 <script src="/jquery-3.1.1.min.js"></script>
 <script>
-    $(function(){
-       alert('请先登录');
-        window.location = '/login';
-    })
+   function delayer(){
+ window.location = "/login";
+}
+jQuery(document).ready(function(){
+ setTimeout('delayer()', 2000);
+});
 </script>
